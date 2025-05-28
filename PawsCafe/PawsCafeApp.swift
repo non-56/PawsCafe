0270b2c9036ae16f42cd56a3cfd53b44df26e8ab
nodoka <cl_s3236541052@soshi.jp>
2025-05-28 15:12:34 +0900

    Initial Commit

---
+++ PawsCafe/PawsCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsCafeApp.swift
+//  PawsCafe
+//
+//  Created by clark on 2025/05/28.
+//
+
+import SwiftUI
+
+@main
+struct PawsCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
