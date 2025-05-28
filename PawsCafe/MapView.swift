cff7ce45ed012695ba5422dbfd04148259e8ae45
nodoka <cl_s3236541052@soshi.jp>
2025-05-28 15:31:07 +0900

    MapView作った

---
+++ PawsCafe/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  PawsCafe
+//
+//  Created by clark on 2025/05/28.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
