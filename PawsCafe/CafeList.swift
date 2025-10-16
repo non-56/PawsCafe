e063e91711a0c70dd9d5079b104443ae4d69718e
f.chika20080331 <f.chika20080331@gmail.com>
2025-10-16 14:54:20 +0900

    カフェ情報

--- PawsCafe/CafeList.swift
+++ PawsCafe/CafeList.swift
@@ -82,10 +82,15 @@ class CafeViewModel: ObservableObject {
 
         // カフェ一覧
         self.allCafes = [
-            Cafe(id: UUID(), name: "にゃんにゃんカフェ", animals:["ネコ"], address:"大阪府", call:"080-XXXX-XXXX", price:"1001円〜2000円", url: URL(string: "https://umeda.example.com")!, imageName: "cafe1", latitude: 34.7025, longitude: 135.4959, tags: ["12歳以下OK","女性のみ"]),
-            Cafe(id: UUID(), name: "わんわんカフェ", animals:["イヌ"], address:"大阪府", call:"080-XXXX-XXXX", price:"2001円〜3000円", url: URL(string: "https://shinsaibashi.example.com")!, imageName: "cafe2", latitude: 34.6723, longitude: 135.5033, tags: []),
-            Cafe(id: UUID(), name: "ぴよぴよカフェ", animals:["鳥類"], address:"大阪府", call:"080-XXXX-XXXX", price:"2001円〜3000円", url: URL(string: "https://tennoji.example.com")!, imageName: "cafe3", latitude: 34.6469, longitude: 135.5132, tags: []),
-            Cafe(id: UUID(), name: "ぶーぶーカフェ", animals:["ブタ"], address:"大阪府", call:"080-XXXX-XXXX", price:"2001円〜3000円", url: URL(string: "https://horie.example.com")!, imageName: "cafe4", latitude: 34.6749, longitude: 135.4949, tags: [])
+            Cafe(id: UUID(), name: "犬カフェsamo", animals:["イヌ"], address:"大阪府", call:"", price:"2001〜3000円", url: URL(string: "https://dogcafe-samo.com/")!, imageName: "cafe1", latitude: 34.7025, longitude: 135.4959, tags: ["12歳以下不可","おやつ可"]),
+                        Cafe(id: UUID(), name: "PREFAM", animals:["イヌ"], address:"大阪府", call:"06-6964-8853", price:"2001円〜3000円", url: URL(string: "https://prefam.jp/")!, imageName: "cafe2", latitude: 34.6723, longitude: 135.5033, tags: ["12歳以下不可"]),
+                        Cafe(id: UUID(), name: "猫カフェMOCHA", animals:["ネコ"], address:"大阪府", call:"", price:"1001〜2000円", url: URL(string: "https://catmocha.jp/")!, imageName: "cafe3", latitude: 34.6469, longitude: 135.5132, tags: ["6歳から入場可","おやつ可","抱っこ可"]),
+                        Cafe(id: UUID(), name: "nekotto", animals:["ネコ"], address:"大阪府", call:"080-4567-0025", price:"1001円〜2000円", url: URL(string: "https://www.nekotto22.com/")!, imageName: "cafe4", latitude: 34.6749, longitude: 135.4949, tags: ["3歳から入場可"]),
+                        Cafe(id: UUID(), name: "cat tail", animals:["ネコ"], address:"大阪府", call:"06-6213-2279", price:"1001〜2000円", url: URL(string: "https://cattail.jp/")!, imageName: "cafe5", latitude: 34.6469, longitude: 135.5132, tags: ["5年生未満不可","おやつ可","抱っこ可"]),
+                        Cafe(id: UUID(), name: "mipig cafe 大阪店", animals:["ブタ"], address:"大阪府", call:"06-6213-6334", price:"1001〜2000円", url: URL(string: "https://mipig.cafe/locations/osaka/")!, imageName: "cafe6", latitude: 34.6469, longitude: 135.5132, tags: ["年齢制限無し","抱っこ不可"]),
+                        
+                        Cafe(id: UUID(), name: "いぬカフェRio 京都店", animals:["ネコ","その他"], address:"京都府", call:"075-366-4281", price:"1001〜2000円", url: URL(string: "https://rio-corp.jp/shops/kyoto/")!, imageName: "cafe7", latitude: 34.6469, longitude: 135.5132, tags: ["抱っこ不可","おやつ可"]),
+
         ]
         
         // 予定
